a2205d7f7620044b7457a27f0000d3983bcd7e70
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2024-04-30 20:17:49 +0530

    change stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const randint = (n) => {
+  return Math.floor(Math.random() * n);
+}
+
+const App = () => {
+  const handle = (e) => {
+    if (e.key === 'Enter') {
+      const answer = parseInt(e.target.value);
+      if (answer === q[0]*q[0]) {
+        e.target.value = '';
+        let x = randint(100);
+        setQ([x,x]);
+        setColor('green');
+        setCount(count+1);
+        localStorage.setItem('score', count+1);
+      } else {
+        e.target.value = '';
+        setColor('red');
+        setCount(count-1);
+        localStorage.setItem('score', count-1);
+      }
+    }
+  }
+
+  const [q, setQ] = useState([randint(100), randint(100)]);
+  const [color, setColor] = useState('white');
+  const [count, setCount] = useState(parseInt(localStorage.getItem('score')) || 0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main>
+      <div className='question' style={{color: color}}>{q[0]}</div>
+      <input type='number' className='answer' onKeyPress={handle} />
+    </main>
+    <div className='counter'>{count}</div>
+    </>
   );
+
 }
 
 export default App;
